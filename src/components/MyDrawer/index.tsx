f7cb46f87ce44e1a7c45afd08bd9520cdb67ec53
Mauricio Goulart <99099560+MauricioGoulartt@users.noreply.github.com>
2023-07-03 13:40:06 -0300

    Update index.tsx

--- src/components/MyDrawer/index.tsx
+++ src/components/MyDrawer/index.tsx
@@ -173,13 +173,13 @@ export default function ResponsiveDrawer() {
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        {}
         <Drawer
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
-            keepMounted: true // Better open performance on mobile.
+            keepMounted: true
           }}
           sx={{
             display: { xs: 'block', sm: 'none' },
